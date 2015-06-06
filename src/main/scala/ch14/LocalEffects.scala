8fc327d36b6b937f20ed798ffa96effbc8e6fe0b
mura-s <seiya.muramatsu1@gmail.com>
2015-06-06 16:27:49 +0900

    Add ch14 answers

--- src/main/scala/ch14/LocalEffects.scala
+++ src/main/scala/ch14/LocalEffects.scala
@@ -54,3 +54,85 @@ object STRef {
 trait RunnableST[A] {
   def apply[S]: ST[S, A]
 }
+
+sealed abstract class STArray[S, A] (implicit manifest: Manifest[A]) {
+  protected def value: Array[A]
+
+  def size: ST[S, Int] = ST(value.length)
+
+  def write(i: Int, a: A): ST[S, Unit] = new ST[S, Unit] {
+    def run(s: S) = {
+      value(i) = a
+      ((), s)
+    }
+  }
+
+  def read(i: Int): ST[S, A] = ST(value(i))
+
+  def freeze: ST[S, List[A]] = ST(value.toList)
+
+  // ex14.1
+  def fill(xs: Map[Int, A]): ST[S, Unit] =
+    xs.foldRight(ST[S, Unit](())) {
+      case ((k, v), st) => st flatMap(_ => write(k, v))
+    }
+
+  def swap(i: Int, j: Int): ST[S, Unit] = for {
+    x <- read(i)
+    y <- read(j)
+    _ <- write(i, y)
+    _ <- write(j, x)
+  } yield ()
+}
+
+object STArray {
+  def apply[S, A: Manifest](sz: Int, v: A): ST[S, STArray[S, A]] =
+    ST(new STArray[S, A] {
+      lazy val value = Array.fill(sz)(v)
+    })
+
+  def fromList[S, A: Manifest](xs: List[A]): ST[S, STArray[S, A]] =
+    ST(new STArray[S, A] {
+      lazy val value = xs.toArray
+    })
+}
+
+object Immutable {
+  def noop[S] = ST[S, Unit](())
+
+  // ex14.2
+  def partition[S](a: STArray[S,Int], l: Int, r: Int, pivot: Int): ST[S,Int] = for {
+    vp <- a.read(pivot)
+    _ <- a.swap(pivot, r)
+    j <- STRef(l)
+    _ <- (l until r).foldLeft(noop[S])((s, i) => for {
+      _ <- s
+      vi <- a.read(i)
+      _  <- if (vi < vp) for {
+        vj <- j.read
+        _  <- a.swap(i, vj)
+        _  <- j.write(vj + 1)
+      } yield () else noop[S]
+    } yield ())
+    x <- j.read
+    _ <- a.swap(x, r)
+  } yield x
+
+  def qs[S](a: STArray[S, Int], l: Int, r: Int): ST[S, Unit] = if (l < r) for {
+    pi <- partition(a, l, r, l + (r - 1) / 2)
+    _ <- qs(a, l, pi - 1)
+    _ <- qs(a, pi + 1, r)
+  } yield () else noop[S]
+
+  def quicksort(xs: List[Int]): List[Int] =
+    if (xs.isEmpty) xs else ST.runST(new RunnableST[List[Int]] {
+      override def apply[S]: ST[S, List[Int]] = for {
+        arr <- STArray.fromList(xs)
+        size <- arr.size
+        _ <- qs(arr, 0, size - 1)
+        sorted <- arr.freeze
+      } yield sorted
+    })
+}
+
+// ex14.3: pass
